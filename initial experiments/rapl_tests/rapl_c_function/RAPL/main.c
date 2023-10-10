f2d9b548506898d395ab439294be91e126eebb26
simaocunha71 <simaopscunha@outlook.pt>
2023-10-10 01:21:51 +0100

    rapl com funçao c funciona - falta corrigir o formato das strings dos prompts

--- initial experiments/rapl_tests/rapl_c_function/RAPL/main.c
+++ initial experiments/rapl_tests/rapl_c_function/RAPL/main.c
@@ -1,14 +1,14 @@
+
 /*
- * Análise e Teste de Software
- * João Saraiva
- * 2016-2017
+ *  Análise e Teste de Software
+ *  João Saraiva
+ *  2016-2017
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <math.h>
 #include <string.h>
 #include "rapl.h"
 
@@ -16,72 +16,70 @@
 
 int main(int argc, char **argv)
 {
-  char command[500] = "";
-  char res[500] = "";
+  char command[500], res[500];
   int ntimes = 1;
   int core = 0;
   int i = 0;
 
 #ifdef RUNTIME
+  clock_t begin, end;
+  double time_spent;
+
   struct timeval tvb, tva;
+
 #endif
+
   FILE *fp;
 
-  if (argc < 4)
-  {
-    fprintf(stderr, "Usage: %s <command> <ntimes> <output_filename>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
+  // printf("Program to be executed: %d",argc);
+  // strcpy( command, "./" );
+  strcat(command, argv[1]);
+  printf("Program to be executed: %s\n", argv[1]);
 
-  strcpy(command, argv[1]);
-  printf("Program to be executed: %s\n", command);
+  strcpy(command, "");
+  strcat(command, argv[1]);
 
   ntimes = atoi(argv[2]);
 
   strcpy(res, argv[3]);
   strcat(res, ".J");
-  printf("Command: %s %d times, Output File: %s\n", command, ntimes, res);
+  printf("Command: %s  %d-times res: %s\n", command, ntimes, res);
 
   printf("\n\n RUNNING THE PARAMETRIZED PROGRAM:  %s\n\n\n", command);
   fflush(stdout);
 
   fp = fopen(res, "w");
-
-  if (fp == NULL)
-  {
-    perror("Error opening output file");
-    exit(EXIT_FAILURE);
+  if (strlen(res) >= 2) {
+    res[strlen(res) - 2] = '\0';
   }
-
   rapl_init(core);
 
-  fprintf(fp, "Program, Package, Core(s), GPU, DRAM?, Time (sec)\n");
+  fprintf(fp, "Program,Package,Core,GPU,DRAM,Time\n");
 
   for (i = 0; i < ntimes; i++)
   {
-    sleep(1);                     // sleep for 1 second
-    fprintf(fp, "%s, ", argv[3]); // write the ID to the csv/.J file
+    sleep(1); // sleep 1 second
+    fprintf(fp, "%s , ", res); //NOTE: retirar o .J da string que vai ser adicionada ao .J
     rapl_before(fp, core);
 
 #ifdef RUNTIME
-    gettimeofday(&tvb, NULL);
+    begin = clock();
+    gettimeofday(&tvb, 0);
 #endif
 
-    if (system(command) == -1)
-    {
-      perror("Error executing command");
-      exit(EXIT_FAILURE);
-    }
+    system(command);
 
 #ifdef RUNTIME
-    gettimeofday(&tva, NULL);
-    double time_spent = (tva.tv_sec - tvb.tv_sec) + (tva.tv_usec - tvb.tv_usec) / 1e6;
+    end = clock();
+    gettimeofday(&tva, 0);
+    //	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    time_spent = (tva.tv_sec - tvb.tv_sec) * 1000000 + tva.tv_usec - tvb.tv_usec;
 #endif
 
     rapl_after(fp, core);
 
 #ifdef RUNTIME
-    fprintf(fp, " %lf\n", time_spent);
+    fprintf(fp, " %G \n", time_spent);
 #endif
   }
 
--- initial experiments/rapl_tests/rapl_c_function/measure.sh
+++ initial experiments/rapl_tests/rapl_c_function/measure.sh
@@ -6,11 +6,14 @@ N_TOKENS=124
 models=(
     "models/llama-2-7b.Q2_K.gguf"
 )
-prompts_filepath="../rapl_tests/rapl_c_function/teste.jsonl"
+
+#prompts_filepath="../rapl_tests/rapl_c_function/prompts/teste.jsonl"
+#prompts_filepath="../rapl_tests/rapl_c_function/prompts/prompts.jsonl"
+
 RESULT_CSV="../rapl_tests/rapl_c_function/result_rapl_c.csv"
 
 # Updates all the prompts so we can use in the command line
-python3 handle_prompts.py $prompts_filepath
+python3 handle_prompts.py $(basename "$prompts_filepath")
 
 cd RAPL/
 make 
@@ -25,17 +28,17 @@ for model in "${models[@]}"; do
         task_id_modified=${task_id//\//_}
 
         # You can customize the command as needed
-        sudo ../rapl_tests/rapl_c_function/RAPL/main "./main -m $model -p \"$prompt\" -n $N_TOKENS -e" $N_TIMES \"$task_id_modified\"
+        sudo ../rapl_tests/rapl_c_function/RAPL/main "./main -m $model -p \"$prompt\" -n $N_TOKENS -e" $N_TIMES $task_id_modified
     done < "$prompts_filepath"
 
-    cat "${TAG}.J" >> "$RESULT_CSV"
+    cat *.J >> "$RESULT_CSV"
 done
 
 # Clean up duplicate rows in the CSV file
 awk -F',' '!seen[$1, $2, $3, $4, $5, $6]++' "$RESULT_CSV" > temp && mv temp "$RESULT_CSV"
 
 # Cleanup
-sudo rm *.J *.log
+#sudo rm *.J *.log
 
 cd ../rapl_tests/rapl_c_function/RAPL/
 make clean
