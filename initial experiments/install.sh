32785af980191a7a7c71af7acb441383bf19eebd
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-11-01 16:23:10 +0000

    script de instalação de bibliotecas

---
+++ initial experiments/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install git
+sudo apt-get install build-essential
+sudo apt-get install make
+sudo apt install python3-pip
+pip3 install llama-cpp-python
+pip3 install human-eval
