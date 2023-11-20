0aade92296cebdf0cde214bf0eefe52df3b327d4
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-11-20 23:18:09 +0000

    bug fix: a script nao executava corretamente o benchmark HumanEval

--- final_version/llama-2-7b.Q2_K.py
+++ final_version/llama-2-7b.Q2_K.py
@@ -8,72 +8,12 @@ import pyRAPL
 #Example: python3 llama2-python_test ""Building a website can be done in 10 simple steps:\nStep 1:" "HumanEval/42"
 
 def print_measure_information(model_name, prompt_id):
-    print(f"\n\nModel: {model_name}")
-    print(f"Prompt: {prompt_id}\n")
-
-def add_human_eval_score_in_csv(csv_file_old, csv_file_new, column_name, value_to_add):
-    """Adiciona um valor numa coluna de um ficheiro CSV já existente"""
-    
-    # Lê o ficheiro CSV de entrada (s/ o HumanEvalScore)
-    with open(csv_file_old, 'r') as csv_in:
-        reader = csv.DictReader(csv_in)
-        header = reader.fieldnames
-
-        # Verifica se a coluna à qual queremos adicionar o score existe no ficheiro CSV de input
-        if column_name not in header:
-            raise ValueError(f"A coluna '{column_name}' não existe no ficheiro CSV de input.")
-
-        # Lê todas as linhas e preenche os valores na coluna específica com o valor dado do score
-        rows = list(reader)
-        for row in rows:
-            if column_name not in row or not row[column_name]:
-                row[column_name] = value_to_add
-
-    # Escreve o ficheiro CSV de output
-    with open(csv_file_new, 'w', newline='') as csv_out:
-        writer = csv.DictWriter(csv_out, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(rows)
-
-def run_human_eval_benchmark(model):
-    """Calcula o score do benchmark HumanEval - neste momento apenas calcula o pass@1 mas mais tarde vou incluir o pass@10 e pass@100"""
-    return_value = None
-
-    # A script do cálculo do score está em human_eval/
-    os.chdir("human_eval")
-
-    # Calcula o(s) score(s) do benchmark HumanEval e coloca o resultado num ficheiro de texto    
-    os.system(f"python3 evaluate_functional_correctness.py data/samples_{model}.jsonl --problem_file=data/HumanEval.jsonl > human_eval_score.txt")
-
-    # Caso exista o ficheiro, iremos fazer parsing do pass@1, pass@10 e pass@100
-    if os.path.exists(f"human_eval_score.txt"):
-        with open(f"human_eval_score.txt", 'r') as file:
-            # Lê o conteúdo do ficheiro
-            lines = file.readlines()
-
-            # Usamos regex para o parsing
-            for line in lines:
-                match = re.search(r"'pass@1': (\d+\.\d+)", line)
-                if match:
-                    # Extraimos o valor de pass@1
-                    pass_value = match.group(1)
-                    return_value = pass_value
-
-    else:
-        print(f"Error: The file 'human_eval_score.txt' was not found.")
-
-    # Apagamos o ficheiro de texto temporário
-    os.system(f"rm human_eval_score.txt")
-
-    # Voltamos a diretoria inicial
-    os.chdir("..")
-
-    # Aqui devolve os scores calculados no parsing (NOTE: no futuro isto irá ser um tuplo do tipo (pass@1, pass@10, pass@100))
-    return return_value
-
+    print("\n\nExecuting:")
+    print(f"> Model: {model_name}")
+    print(f"> Prompt: {prompt_id}\n")
 
 def generate_samples(model, output, label):
-    """Gera o ficheiro das samples deste modelo"""
+    """Gera o ficheiro das samples deste modelo (Benchmark: HumanEval)"""
 
     label = label.replace("/", "_")
 
@@ -164,8 +104,6 @@ with open(FILENAME, 'a', newline='') as csv_file:
     #NOTE: colocar em J e em S
     csv_writer.writerow(["llama-2-7b.Q2_K", label, execution_time, measure.result.pkg[0], measure.result.dram[0]])
 
-human_eval_score = run_human_eval_benchmark("llama-2-7b.Q2_K")
-add_human_eval_score_in_csv(FILENAME, FILENAME, "HumanEval pass@1", human_eval_score)
 """
 Descrição das colunas CSV:
 
