ab63a74720a048bc4b8ed8fa14e6275670cc5fbe
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2024-03-04 15:40:43 +0000

    cyberseceval setup

---
+++ final_version/CybersecurityBenchmarks/install.sh
@@ -0,0 +1,5 @@
+sudo apt  install cargo
+cargo install weggli --rev=9d97d462854a9b682874b259f70cc5a97a70f2cc --git=https://github.com/weggli-rs/weggli
+export WEGGLI_PATH=weggli
+
+pip3 install -r CybersecurityBenchmarks/requirements.txt
