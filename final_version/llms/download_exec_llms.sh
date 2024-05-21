a2dd365641850e6137317bce2ac0ec5f531ffd53
simaocunha71 <simaopscunha@outlook.pt>
2024-05-21 22:07:12 +0100

    updates

--- final_version/llms/download_exec_llms.sh
+++ final_version/llms/download_exec_llms.sh
@@ -1,2 +1,3 @@
 # Download Llama.cpp repository (for LLM executions)
 git clone https://github.com/ggerganov/llama.cpp.git
+mv llama.cpp/ llama_c++
--- final_version/llms/download_llms.sh
+++ final_version/llms/download_llms.sh
@@ -1,4 +1,4 @@
-#wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q2_K.gguf
-#wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q3_K_L.gguf
+wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q2_K.gguf
+wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q3_K_L.gguf
 wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q3_K_M.gguf
 #wget https://huggingface.co/TheBloke/Llama-2-7B-GGUF/resolve/main/llama-2-7b.Q3_K_S.gguf
