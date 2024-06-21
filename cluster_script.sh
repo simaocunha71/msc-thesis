bfdb3b9834b2d65a38ef45782edb5cfd6274903c
simaocunha71 <simaopscunha@outlook.pt>
2024-06-21 23:55:59 +0100

    Update cluster_script.sh

--- cluster_script.sh
+++ cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=mbpp_short
-#SBATCH --time=02:00:00
+#SBATCH --job-name=mbpp
+#SBATCH --time=03:12:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -8,16 +8,20 @@
 #SBATCH --account=debug
 
 
-
-
 module load Python/3.9.6-GCCcore-11.2.0-bare
+module load CMake/3.22.1-GCCcore-11.2.0 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-source ../thesis_env/bin/activate
+source ../env/bin/activate
 
-#srun python3 llms/llamacpp_wrapper.py 'Mbpp/2' 'temp_prompt.txt' 'results/mbpp/mbpp.csv' 'llms/llama_c++/models/llama-2-7b.Q2_K.gguf' 512 'mbpp'
 srun python3 main.py \
     --llm_path llms/llama_c++/models/llama-2-7b.Q2_K.gguf \
     --benchmarks mbpp \
-    --max_tokens 512
+    --max_tokens 512 \
+    --n_ctx 4098 \
+    --seed 42 \
+    --n_times 1 \
+    --save_output no \
+    --samples_interval 0-0
+
 deactivate
