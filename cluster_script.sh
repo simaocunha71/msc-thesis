6cb88bfaa4f48d9c24c090912b830d594ff40e6f
simaocunha71 <simaopscunha@outlook.pt>
2024-06-24 17:23:44 +0100

    cluster_script updated and requirements.txt updated

--- cluster_script.sh
+++ cluster_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=mbpp
-#SBATCH --time=03:12:00
+#SBATCH --job-name=mbpp_sanitized
+#SBATCH --time=3-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -8,11 +8,10 @@
 #SBATCH --account=debug
 
 
-module load Python/3.9.6-GCCcore-11.2.0-bare
-module load CMake/3.22.1-GCCcore-11.2.0 
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
 source ../env/bin/activate
+module load Python/3.9.6-GCCcore-11.2.0-bare
+module load CMake/3.22.1-GCCcore-11.2.0
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 srun python3 main.py \
     --llm_path llms/llama_c++/models/llama-2-7b.Q2_K.gguf \
@@ -21,7 +20,8 @@ srun python3 main.py \
     --n_ctx 4098 \
     --seed 42 \
     --n_times 1 \
-    --save_output no \
-    --samples_interval 0-0
+    --save_output yes \
+    --samples_interval 0-0 \
+    --enable_sanitize no
 
 deactivate
