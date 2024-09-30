5b81ac44a71c35701d5b353312392ee5cdf3b618
simaocunha71 <simaopscunha@outlook.pt>
2024-09-30 22:06:04 +0100

    Update download_prompt_files.sh

--- prompts/download_prompt_files.sh
+++ prompts/download_prompt_files.sh
@@ -1,23 +1,29 @@
+#!/bin/bash
+
+# Create and navigate to the humaneval_x directory
+mkdir -p humaneval_x
+cd humaneval_x/ || exit
+
 # Download HumanEval-X datasets
-mkdir humaneval_x
-cd humaneval_x/
 wget "https://github.com/THUDM/CodeGeeX/raw/main/codegeex/benchmark/humaneval-x/cpp/data/humaneval_cpp.jsonl.gz"
 wget "https://github.com/THUDM/CodeGeeX/raw/main/codegeex/benchmark/humaneval-x/go/data/humaneval_go.jsonl.gz"
 wget "https://github.com/THUDM/CodeGeeX/raw/main/codegeex/benchmark/humaneval-x/java/data/humaneval_java.jsonl.gz"
 wget "https://github.com/THUDM/CodeGeeX/raw/main/codegeex/benchmark/humaneval-x/js/data/humaneval_js.jsonl.gz"
 wget "https://github.com/THUDM/CodeGeeX/raw/main/codegeex/benchmark/humaneval-x/python/data/humaneval_python.jsonl.gz"
 gzip -d *.gz
-cd ..
+cd .. || exit
+
+# Create and navigate to the mbpp directory
+mkdir -p mbpp
+cd mbpp/ || exit
 
 # Download MBPP+ dataset
-mkdir mbpp
-cd mbpp/
 wget "https://github.com/evalplus/mbppplus_release/releases/download/v0.2.0/MbppPlus.jsonl.gz"
 gzip -d *.gz
-cd ..
+cd .. || exit
 
 # Download CyberSecEval datasets
 wget https://github.com/meta-llama/PurpleLlama/archive/refs/heads/main.zip
 unzip main.zip 
 mv PurpleLlama-main/CybersecurityBenchmarks/datasets ./cyberseceval/
-rm -rf main.zip  PurpleLlama-main/
+rm -rf main.zip PurpleLlama-main
